2f3e2cc79d32f8cd113af09f3bce66003d5e7bf6
ttamura01git config --global user.email ttamura010164@me.com~git remote add origin https://github.com/ttamura01/git-fed_rate_cut.gitgit push origin mainls <ttamura010164@me.com>
2026-02-11 10:28:28 -0500

    updates with the latest data

--- pittsburgh_temp.R
+++ pittsburgh_temp.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(lubridate)
 library(glue)
 library(ggtext)
+library(gt)
+library(gtExtras)
 
 setwd("/Users/takayukitamura/Documents/R_Computing/pittsburgh_climate_r")
 
@@ -9,15 +11,15 @@ setwd("/Users/takayukitamura/Documents/R_Computing/pittsburgh_climate_r")
 # 1) Read data
 # -----------------------------
 
-#pit <- read_csv("pit.csv")
+pit <- read_csv("pit.csv")
 # # 
-#updates <- read_csv("/Users/takayukitamura/Desktop/4224432.csv") %>%
+updates <- read_csv("/Users/takayukitamura/Desktop/4227090.csv") %>%
 # select(-SNWD) %>%
-#filter(DATE >= "2026-02-01")
+filter(DATE >= "2026-02-04")
 # # 
-#pit <- rbind(pit, updates)
+pit <- rbind(pit, updates)
 # # 
-#write_csv(pit, "pit.csv")
+write_csv(pit, "pit.csv")
 
 pittsburgh <- read_csv("pit.csv") %>% 
   rename_all(tolower)
